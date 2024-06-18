96db5bd9209d60e5b734926e30b902be3a5172ab
Sutomo <yogasutomo67@gmail.com>
2024-06-19 02:57:13 +0700

    message: change leanguage back

--- src/services/benefitList.js
+++ src/services/benefitList.js
@@ -1,14 +1,14 @@
 const benefitList = {
-    'Apel': "Apples are good for the stomach because they have fiber in them, and there are also substances called antioxidants that protect our bodies from disease.",
-    'Brokoli': "Broccoli is like a hero vegetable because it contains a lot of vitamin C that keeps our bodies strong and protects us from diseases.",
-    'Jeruk': "Oranges are like vitamins in fruit form! They help us not get sick and make our hearts happy.",
-    'Kangkung': "Kale is like a witch vegetable because it can make our bodies strong and healthy, and keep us safe from diseases.",
-    'Mangga': "Mangoes are like superheroes for our eyes as they contain vitamin A and also help our stomach to work properly.",
-    'Pisang': "Bananas are like a protective food for our heart and also make our bones strong.",
-    'Strawberry': "Strawberries are our best friends that help our bodies stay healthy and strong, and keep us safe from diseases.",
-    'Terong': "Eggplant is like a benevolent witch, as it protects our body from diseases and also helps our stomach to work properly.",
-    'Toge': "Toge is a superfood that makes our bodies big and strong, and helps our brains think well.",
-    'Wortel': "Carrots are the guardian vegetable of our eyes, and they also help our stomachs work properly to keep us healthy and strong."
+    'Apel': "Apel bagus untuk perut karena ada serat di dalamnya, dan juga ada zat bernama antioksidan yang menjaga tubuh kita dari penyakit.",
+    'Brokoli': "Brokoli seperti sayuran pahlawan karena mengandung banyak vitamin C yang membuat tubuh kita kuat dan melindungi kita dari penyakit.",
+    'Jeruk': "Jeruk itu seperti vitamin dalam bentuk buah! Itu membantu kita tidak sakit dan membuat hati kita bahagia.",
+    'Kangkung': "Kangkung itu seperti sayuran penyihir karena bisa membuat tubuh kita kuat dan sehat, serta menjaga kita dari penyakit.",
+    'Mangga': "Mangga itu seperti superhero untuk mata kita karena mengandung vitamin A dan juga membantu perut kita untuk bekerja dengan baik.",
+    'Pisang': "Pisang seperti makanan pelindung bagi jantung kita dan juga membuat tulang kita kuat.",
+    'Strawberry': "Strawberry adalah teman baik kita yang membantu tubuh kita tetap sehat dan kuat, serta menjaga kita dari penyakit.",
+    'Terong': "Terong itu seperti penyihir baik hati, karena melindungi tubuh kita dari penyakit dan juga membantu perut kita untuk bekerja dengan baik.",
+    'Toge': "Toge adalah makanan super yang membuat tubuh kita besar dan kuat, serta membantu otak kita berpikir dengan baik.",
+    'Wortel': "Wortel adalah sayuran penjaga mata kita, dan juga membantu perut kita bekerja dengan baik agar kita tetap sehat dan kuat."
 };
 
 module.exports = benefitList
